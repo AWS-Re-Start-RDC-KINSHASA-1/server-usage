a3e6a91cf9b37865ae7c3dc2ebf0232159229f71
Henock BARAKAEL <barahenock@gmail.com>
2024-02-15 15:06:47 +0100

    Suppresion des utilisateurs

--- exercice2.py
+++ exercice2.py
@@ -5,6 +5,6 @@ def remove_user():
     confirm = "N"
     while confirm != "Y":
         username = input("Indiquez le nom de l'utilisateur à supprimer: ")
-        print("Supprimer l’utilisateur : '" + username + "'? (Y/N)")
+        print("Supprimer l'utilisateur : '" + username + "'? (Y/N)")
         confirm = input().upper()
     os.system("sudo userdel -r " + username) 
