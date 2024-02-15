8cebf7c567a583e75c43f7db6fb6c4f5dd2b6f50
Henock BARAKAEL <barahenock@gmail.com>
2024-02-15 15:10:07 +0100

    update master branch

--- sysadmin.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def new_user():
-    confirm = "N"
-    while confirm != "Y":
-        username = input("Indiquez le nom de l'utilisateur à ajouter: ")
-        print("Utiliser username '" + username + "'? (Y/N)")
-        confirm = input().upper()
-    os.system("sudo adduser " + username)
